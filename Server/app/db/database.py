964eba83ee4a2db5386555ecbf617f44a8323ce7
AncyJoseph01 <meowlytics@gmail.com>
2025-08-04 23:00:53 +0100

    updated the new postgresql db

--- Server/app/db/database.py
+++ Server/app/db/database.py
@@ -1,7 +1,7 @@
 from databases import Database
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
-from core.config import settings
+from ..core.config import settings
 
 database = Database(settings.DATABASE_URL)
 Base = declarative_base()
